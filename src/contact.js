60c7a668322884d07f06bdae839475660b3412a6
chrisgth <ksemkus00@gmail.com>
2022-05-06 16:24:16 +0300

    Finish rough styling of website

--- src/contact.js
+++ src/contact.js
@@ -3,17 +3,54 @@ export default function contact() {
     content.innerHTML = ''
     
     const headline = document.createElement('h2')
-    const image = document.createElement('img')
-    const descriptionDiv = document.createElement('div')
-    const description = document.createElement('p')
+    const contactDesc = document.createElement('p')
+    const contactForm = document.createElement('form')
 
     headline.textContent = 'Contact'
-    image.src = 'https://img.freepik.com/free-photo/restaurant-interior_1127-3394.jpg?t=st=1651647562~exp=1651648162~hmac=1032a7526c3c730d1cab48700282bf1233beb91222f7969c37f23c3b9c2a43e7&w=996'
-    description.textContent = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui totam, ut mollitia optio fugit dolor id. Veritatis adipisci placeat sed.'
-
-    descriptionDiv.appendChild(description)
+    contactDesc.textContent = 'Want to send us a message? Fill out the form below!'
     content.appendChild(headline)
-    content.appendChild(image)
-    content.appendChild(description)
+    content.appendChild(contactDesc)
+
+    for (let i=0; i<3; i++){
+        let formDiv = document.createElement('div')
+        let formLabel = document.createElement('label')
+        let formInput = document.createElement('input')
+
+        formInput.type = 'text'
+        formLabel.id = `formLabel${i}`
+        formLabel.htmlFor = `formInput${i}`
+        formInput.id = `formInput${i}`
+
+        if (i===0) {
+            formLabel.textContent = 'First Name:'
+        } else if (i===1) {
+            formLabel.textContent = 'Last Name:'
+        } else if (i===2) {
+            formLabel.textContent = 'E-Mail:'
+        }
+        formDiv.appendChild(formLabel)
+        formDiv.appendChild(formInput)
+        contactForm.appendChild(formDiv)
+    }
+    const formDiv3 = document.createElement('div')
+    const formLabel3 = document.createElement('label')
+    const formTxtArea = document.createElement('textarea')
+    formDiv3.id = 'formDiv3'
+    formTxtArea.id = 'formTxtArea'
+    formLabel3.htmlFor = 'formTxtArea'
+    formLabel3.textContent = 'Message:'
+    formDiv3.appendChild(formLabel3)
+    formDiv3.appendChild(formTxtArea)
+    contactForm.appendChild(formDiv3)
+
+    content.appendChild(contactForm)
+
+    const formButton = document.createElement('button')
+    formButton.type = 'button'
+    formButton.id = 'formbtn'
+    formButton.textContent = 'Submit'
+
+    content.appendChild(formButton)
+
     console.log('checkcontact')
 }
